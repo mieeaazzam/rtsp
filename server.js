cfd971774a34408e4e75ebcafb5f2b05354ad8aa
mieeaazzam <46929141+mieeaazzam@users.noreply.github.com>
2021-05-06 02:19:16 +0200

    Update server.js
    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Stream = require('./src/videoStream')
 
 const stream = new Stream({
     name: 'RtspStreamTest',
-    url: 'rtsp://10.0.0.22:554/',
+    url: 'rtsp://10.0.1.88:554/',
     port: 10086
 })
 
